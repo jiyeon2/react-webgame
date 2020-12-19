18794bb0c2abc45512d7853b8fedf7b1a0c5b688
jiyeon lee <a1919361@gmail.com>
2020-12-19 20:30:59 +0900

    review webpack config and hot reload

---
+++ 3_baseball/Baseball-class.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Baseball extends React.Component{
+    render() {
+        return (
+        <>
+        hititidfsds
+        </>
+        );
+    }
+}
+
+export default Baseball;
